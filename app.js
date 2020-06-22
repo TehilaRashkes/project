4d6e435fb41df90fccb4852b1be1fd8a137b2a5b
tehila_roni berlin <roni25536@gmail.com>
2020-06-22 23:21:18 +0300

    first

---
+++ app.js
@@ -0,0 +1,10 @@
+const express=reuquire('express');
+const app=express();
+
+app.get('/', (req,res)=>{
+    res.send("Hello world");
+})
+
+app.listen(3000,()=>{
+    console.log("server is listening on port 3000");
+})
